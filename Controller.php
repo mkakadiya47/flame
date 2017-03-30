f7a7439c6d95d8a06d9a6bed27e19ec04e9767bc
mkakadiya47 <mkakadiya47@gmail.com>
2017-03-30 23:36:28 +0530

    Change code

--- Controller.php
+++ Controller.php
@@ -1,25 +1,275 @@
 <?php
-header('Content-Type: application/json');
-error_reporting(E_ALL); 
+class Controller extends Database {
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-set_time_limit(727379969);
-ini_set('max_execution_time', 84400);
-ini_set('max_input_time', 84400);
-ini_set('post_max_size', '5120M');
-ini_set('max_file_uploads', 100);
-ini_set('upload_max_filesize', '5120M');
+	public $site_url;
 
-ini_set('default_socket_timeout', 84400);
-ini_set('session.gc_maxlifetime', 84400);
-ini_set('memory_limit','5120M');
-extract($_POST);
-extract($_GET);
-extract($_REQUEST);
-include 'PushNotification.php';
-	
-               $pushNotification = new PushNotification();
-               $userData = json_decode($_REQUEST['userData'], true);
-	       $pushNotification->ios($userData, $userData['r_device_token']);
+	public function __construct() {
+		parent::__construct();
+		$this->site_state = 0;
+ 		$this->site_url = SITE_URL;
+ 		$this->mailManager = new MailManager();
+ 	}
+
+	public function passwordMd5($password){
+		return md5($password);
+	}
+   
+    public function login() {
+		if($_REQUEST["email"]!='' && $_REQUEST["password"]!='')
+		{ 
+			$where = 'WHERE (u.email = :email or u.username = :email) AND u.password = :password';
+			$bind = array(
+				':email' => urldecode($_REQUEST["email"]),
+				':password' => $this->passwordMd5(urldecode($_REQUEST["password"]))
+			);
+			$query = 'SELECT u.id, u.email, u.first_name, u.last_name
+			FROM user u ' . $where;
+			$this->query($query);
+			$this->bind($bind);
+			$this->execute();
+			if ($this->rowCount() > 0) {
+				$result1 = $this->single();
+				$this->api_status = '1';
+				$this->api_message = 'Login successfully';
+				$this->api_data = $result1;
+				$_SESSION['username'] = urldecode($_REQUEST["email"]);
+				$_SESSION['username'] = $result1["id"];
+			}else{
+				$this->api_status = '0';
+				$this->api_message = 'Please check email and password';
+				$this->api_data = '';
+			}
+		}else{
+			$this->api_status = '0';
+			$this->api_message = 'Please provide required value';
+			$this->api_data = '';
+		} 
+		return $this->response();
+ 	}
+
+   	public function register() {
+		if($_REQUEST["email"]!='')
+	  	{ 
+		 	$where = 'WHERE u.email = :email or u.username = :username';
+			$bind = array(
+				':email' => urldecode($_REQUEST["email"]),
+				':username' => urldecode($_REQUEST["username"])
+			);
+			$query = 'SELECT u.email, u.username
+							FROM user u ' . $where;
+			$this->query($query);
+			$this->bind($bind);
+			$this->execute();
+			if ($this->rowCount() > 0) {
+				$this->api_status = '0';
+				$this->api_message = 'Email or username allready exists';
+				$this->api_data = '';
+			} else {
+				$query = 'INSERT INTO user SET 
+							first_name = :first_name,
+							last_name = :last_name, 
+							email = :email, 
+							username = :username, 
+							password = :password,
+							address = :address';
+				$bind = array(
+					':email' => urldecode($_REQUEST["email"]),
+					':password' => $this->passwordMd5(urldecode($_REQUEST["password"])),
+					':username' => $_REQUEST['username'],
+					':first_name' => $_REQUEST['first_name'],
+					':last_name' => $_REQUEST['last_name'],
+					':address' => $_REQUEST['address'],
+				);
+				$this->query($query);
+				$this->bind($bind);
+				$this->execute();
+				$this->api_status = '1';
+				$this->api_message = 'Register successfully';
+				$this->api_data = '';
+	 		}
+	  	}else{
+			$this->api_status = '0';
+			$this->api_message = 'Please provide required value';
+			$this->api_data = '';
+		} 
+ 		return $this->response();
+ 	}
+   	public function changePassword() {
+		if($_REQUEST["id"]!='')
+	  	{ 
+		 	$where = 'WHERE u.id = :id';
+			$bind = array(
+				':id' => urldecode($_REQUEST["id"])
+			);
+			$query = 'SELECT u.email, u.password
+							FROM user u ' . $where;
+			$this->query($query);
+			$this->bind($bind);
+			//$this->debugQuery($query, $bind); exit;
+			$this->execute();
+			$result = $this->single();
+			if ($this->rowCount() > 0) {
+				if(isset($_REQUEST["password"])
+					&& isset($_REQUEST["old_password"])
+					&& $result['password'] == $this->passwordMd5($_REQUEST["old_password"])){
+					$query = 'UPDATE user SET password = :password where id=:id';
+					$bind = array(
+						':id' => urldecode($_REQUEST["id"]),
+						':password' => $this->passwordMd5(urldecode($_REQUEST["password"]))
+					);
+					$this->query($query);
+					$this->bind($bind);
+					$this->execute();
+					$this->api_status = '1';
+					$this->api_message = 'Password updated successfully';
+					$this->api_data = 'null';
+				}else {
+					$this->api_status = '0';
+					$this->api_message = 'Password not matched';
+					$this->api_data = '';
+		 		}
+			} else {
+
+				$this->api_status = '0';
+				$this->api_message = 'User not found';
+				$this->api_data = '';
+	 		}
+	  	}else{
+			$this->api_status = '0';
+			$this->api_message = 'Please provide required value';
+			$this->api_data = '';
+		} 
+ 		return $this->response();
+ 	}
+
+   	public function editUser() {
+		if($_REQUEST["id"]!='')
+	  	{ 
+		 	$where = 'WHERE id = :id';
+			
+			$bind = array();
+			$query = 'UPDATE user SET 
+						first_name = :first_name, 
+						last_name = :last_name, 
+						email = :email, 
+						username = :username,
+						address =:address';
+			$query .= $where;
+			$bind[':id'] = urldecode($_REQUEST["id"]);
+			$bind[':email'] = urldecode($_REQUEST["email"]);
+			$bind[':username'] = $_REQUEST['username'];
+			$bind[':first_name'] = $_REQUEST['first_name'];
+			$bind[':last_name'] = $_REQUEST['last_name'];
+			$bind[':address'] = $_REQUEST['address'];
+			$this->query($query);
+			$this->bind($bind);
+			$this->execute();
+			$where = ' WHERE u.id = '.urldecode($_REQUEST["id"]);
+			$query = 'SELECT u.id, u.first_name, u.last_name, u.email, u.username, u.address
+				FROM user u ' . $where;
+			$this->query($query);
+			$this->execute();
+			$result = $this->single();
+			$this->api_status = '1';
+			$this->api_message = 'User updated successfully';
+			$this->api_data = $result;
+	  	}else{
+			$this->api_status = '0';
+			$this->api_message = 'Please provide required value';
+			$this->api_data = '';
+		} 
+ 		return $this->response();
+ 	}	
+
+  	public function randomPassword() 
+  	{
+		$alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
+		$pass = array(); //remember to declare $pass as an array
+		$alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
+		for ($i = 0; $i < 6; $i++) {
+			$n = rand(0, $alphaLength);
+			$pass[] = $alphabet[$n];
+		}
+		return implode($pass); //turn the array into a string
+	}
+
+  	public function forgotPassword()
+  	{
+	  	if($_REQUEST['email']!='')
+	  	{
+			$where = ' WHERE (u.email = :email)';
+			$bind = array(
+			  ':email' => urldecode($_REQUEST["email"]),
+			);
+			$query = 'SELECT u.id, u.email FROM user u ' . $where;
+			$this->query($query);
+			$this->bind($bind);
+			$this->execute();
+		  	if ($this->rowCount() > 0) {
+			 	$result = $this->single();
+			 	if ($result["email"] <> '') 
+			 	{
+					//$from = "info@local.videoapi.mk";
+					$subject = "Forgot Password";
+					$to = $result['email'];
+					$new_pass = $this->randomPassword();
+					
+					$message =  $this->generateEmailtemplate($new_pass); 
+					$updquery = 'UPDATE user SET password = "'.$this->passwordMd5($new_pass).'" where email ="'.$_REQUEST['email'].'"';
+					$this->query($updquery);
+					$this->execute();
+
+					$this->mailManager->sendEmail($to, $subject, $message);
+					// var_dump($tt);
+					$this->api_data = '';
+					$this->api_status = '1';
+					$this->api_message = "Your Passoword is sent to your register email address.";
+			 	} 
+		 	} else {
+				$this->api_status = '0';
+				$this->api_message = EMAIL_NOT_EXIST;
+				$this->api_data = '';
+				return $this->response();
+		  	}
+   	  	}else{
+	  		 $this->api_status = '0';
+			 $this->api_message = "Please provide required value";
+			 $this->api_data = '';
+	  	}
+  	}
+ 
+   	public function response() { // Prepare a json response to the App
+        $api_status = '';
+        if ($this->api_status != '')
+            $api_status = $this->api_status;
+
+        $api_message = '';
+        if ($this->api_message != '')
+            $api_message = $this->api_message;
+
+        $api_data = '';
+        if ($this->api_data != '')
+            $api_data = $this->api_data;
+		
+				
+	 	$this->api_return = array(
+			 'status' => $api_status,
+			 'message' => $api_message,
+			 'flame_image_url' => FLAME_IMAGE_DOWNLOAD_URL,
+			 'flame_audio_url' => FLAME_AUDIO_DOWNLOAD_URL,
+			 'flame_video_url' => FLAME_VIDEO_DOWNLOAD_URL,
+			 'data' => $api_data,
+		 );
+         return json_encode($this->api_return);
+  	}
+ 
+   	public function generateUniqueName() {
+        return uniqid() . '_' . md5(mt_rand());
+  	}
+  	
+  	public function generateEmailtemplate($new_pass="")
+  	{
+		$output='<h2>You recently requested a new password.</h2><b>Your new password is : </b>'.$new_pass.'</b>';
+		return $output;
+	}
+}
--- config.php
+++ config.php
@@ -1,18 +1,16 @@
 <?php
-define("SITE_URL", "http://flame.appsextent.com/");
+define("SITE_URL", "62.75.151.95");
 define("HOST", "localhost");
 define("DBUSER", "root");
 define("DBPASS", "root");
 define("DBNAME", "flame");
 define("TOKEN", "flame");
-define("FLAME_IMAGE_DOWNLOAD_URL", SITE_URL."/uploads/image/");
+define("FLAME_IMAGE_DOWNLOAD_URL", SITE_URL."/uploads/images/");
 define("FLAME_AUDIO_DOWNLOAD_URL", SITE_URL."/uploads/audio/");
 define("FLAME_VIDEO_DOWNLOAD_URL", SITE_URL."/uploads/video/");
-define("USER_IMAGE_DOWNLOAD_URL", SITE_URL."/uploads/image/");
-define("FLAME_IMAGE_UPLOAD_URL", __DIR__."/uploads/image/");
+define("FLAME_IMAGE_UPLOAD_URL", __DIR__."/uploads/images/");
 define("FLAME_AUDIO_UPLOAD_URL", __DIR__."/uploads/audio/");
 define("FLAME_VIDEO_UPLOAD_URL", __DIR__."/uploads/video/");
-define("USER_IMAGE_UPLOAD_URL", __DIR__."/uploads/image/");
 define('FROM_EMAIL', 'flameapplication@gmail.com');
 define('FROM_EMAIL_TITLE', 'Flame Application');
 define('EMAIL_PASSWORD', 'flameapplication@#');
